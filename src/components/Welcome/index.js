c8d61af2b569a4a008b412d2a9bfdb287a7612cf
Surya T N <suryatej.nim@yahoo.com>
2022-08-31 14:56:14 +0000

    Implemented class component Welcome in index.js and adjusted the CSS styling in index.css

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,1 +1,34 @@
-// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+export default class Welcome extends Component {
+  state = {
+    currentButtonText: 'Subscribe',
+  }
+
+  onButtonClick = () =>
+    this.setState(previousButtonTextState => ({
+      currentButtonText:
+        previousButtonTextState.currentButtonText === 'Subscribe'
+          ? 'Subscribed'
+          : 'Subscribe',
+    }))
+
+  render() {
+    const {currentButtonText} = this.state
+
+    return (
+      <div className="welcome-bg-container">
+        <h1 className="welcome-header">Welcome</h1>
+        <p className="welcome-message">Thank you! Happy Learning</p>
+        <button
+          type="button"
+          className="subscribe-button"
+          onClick={this.onButtonClick}
+        >
+          {currentButtonText}
+        </button>
+      </div>
+    )
+  }
+}
